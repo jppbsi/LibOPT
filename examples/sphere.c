7be31c4976802dab87d7f53e1ea76223250bf91b
Joao Papa <papa@Joaos-MacBook-Air-2.local>
2016-06-02 20:52:14 -0300

    changed examples/sphere.c

--- examples/sphere.c
+++ examples/sphere.c
@@ -8,7 +8,21 @@ int main(){
     SearchSpace *s = NULL;
     int i;
     
-    s = ReadSearchSpaceFromFile("examples/model_files/pso_model.txt", _PSO_); /* It reads a model file and creates a search space considering all the inputed parameters. */
+    s = CreateSearchSpace(10, 2, _PSO_); /* It creates a search space with 10 agents for a 2-D problem. We are going to use PSO to solve it. */
+    
+    /* It initializes PSO parameters */
+    s->w = 1.4;
+    s->c1 = 1.4;
+    s->c2 = 0.6;
+    s->iterations = 10;
+    
+    /* It initalizes the boundaries of decision variables within the range [-5.12,5.12]*/
+    for (i = 0; i < s->n; i++){
+        s->LB[i] = -5.12;
+        s->UB[i] = 5.12;
+    }
+    /******/
+    
     InitializeSearchSpace(s); /* It initalizes the search space */
     
     runPSO(s, Sphere); /* It minimizes function Sphere */
