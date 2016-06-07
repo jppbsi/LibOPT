b7ac3f2d97df01f760b3e1b9e389ff258ff3c339
gugarosa <gth.rosa@uol.com.br>
2016-06-07 10:50:17 -0300

    Added all benchmarking functions header. Reordered alphabetically functions.c.

--- include/function.h
+++ include/function.h
@@ -3,8 +3,171 @@
 
 #include "common.h"
 
-double Sphere(Agent *a, ...); /* It computes the equation of a 2-D sphere */
-double Rosenbrock(Agent *a, ...); /* It computes the Rosenbrock's function */
+double Ackley_First(Agent *a, ...); /* It computes the 1st Ackley's function */
+double Ackley_Second(Agent *a, ...); /* It computes the 2nd Ackley's function */
+double Ackley_Third(Agent *a, ...); /* It computes the 3rd Ackley's function */
+double Ackley_Forth(Agent *a, ...); /* It computes the 4th Ackley's function */
+double Adjiman(Agent *a, ...); /* It computes the Adjiman's function */
+double Alpine_First(Agent *a, ...); /* It computes the 1st Alpine's function */
+double Alpine_Second(Agent *a, ...); /* It computes the 2nd Alpine's function */
+double Brad(Agent *a, ...); /* It computes the Brad's function */
+double Bartels_Conn(Agent *a, ...); /* It computes the Bartels' Conn function */
+double Beale(Agent *a, ...); /* It computes the Beale's function */
+double Biggs_EXP2(Agent *a, ...); /* It computes the 2-D Biggs' Exponential function */
+double Biggs_EXP3(Agent *a, ...); /* It computes the 3-D Biggs' Exponential function */
+double Biggs_EXP4(Agent *a, ...); /* It computes the 4-D Biggs' Exponential function */
+double Biggs_EXP5(Agent *a, ...); /* It computes the 5-D Biggs' Exponential function */
+double Biggs_EXP6(Agent *a, ...); /* It computes the 6-D Biggs' Exponential function */
+double Bird(Agent *a, ...); /* It computes the Bird's function */
+double Bohachevsky_First(Agent *a, ...); /* It computes the 1st Bohachevsky's function */
+double Bohachevsky_Second(Agent *a, ...); /* It computes the 2nd Bohachevsky's function */
+double Bohachevsky_Third(Agent *a, ...); /* It computes the 3rd Bohachevsky's function */
+double Booth(Agent *a, ...); /* It computes the Booth's function */
+double Box_Betts(Agent *a, ...); /* It computes the Box-Betts Quadratic Sum function */
+double Branin_RCOS(Agent *a, ...); /* It computes the 1st Branin RCOS function */
+double Branin_RCOS2(Agent *a, ...); /* It computes the 2nd Branin RCOS function */
+double Brent(Agent *a, ...); /* It computes the Brent's function */
+double Brown(Agent *a, ...); /* It computes the Brown's function */
+double Bukin_Second(Agent *a, ...); /* It computes the 2nd Bukin's function */
+double Bukin_Forth(Agent *a, ...); /* It computes the 4th Bukin's function */
+double Bukin_Sixth(Agent *a, ...); /* It computes the 6th Bukin's function */
+double Three_HumpCamel(Agent *a, ...); /* It computes the Three-Hump Camel function */
+double Six_HumpCamel(Agent *a, ...); /* It computes the Six-Hump Camel function */
+double Chen_Bird(Agent *a, ...); /* It computes the Chen's Bird function */
+double Chen_V(Agent *a, ...); /* It computes the Chen's V function */
+double Chichinadze(Agent *a, ...); /* It computes the Chichinadze's function */
+double Chung_Reynolds(Agent *a, ...); /* It computes the Chung Reynolds' function */
+double Cola(Agent *a, ...); /* It computes the Cola's function */
+double Colville(Agent *a, ...); /* It computes the Colville's function */
+double Corana(Agent *a, ...); /* It computes the Corana's function */
+double Cosine_Mixture(Agent *a, ...); /* It computes the Cosine Mixture function */
+double Cross_Tray(Agent *a, ...); /* It computes the Cross-in-Tray function */
+double Csendes(Agent *a, ...); /* It computes the Csendes' function */
+double Cube(Agent *a, ...); /* It computes the Cube's function */
+double Damavandi(Agent *a, ...); /* It computes the Damavandi's function */
+double Deb_First(Agent *a, ...); /* It computes the 1st Deb's function */
+double Deb_Third(Agent *a, ...); /* It computes the 3rd Deb's function */
+double Deckkers_Aarts(Agent *a, ...); /* It computes the Deckkers'-Aarts function */
+double deVilliers_Glasser_First(Agent *a, ...); /* It computes the 1st deVilliers' Glasser function */
+double deVilliers_Glasser_Second(Agent *a, ...); /* It computes the 2nd deVilliers' Glasser function */
 double Dixon_Price(Agent *a, ...); /* It computes the Dixon's-Price function */
+double Dolan(Agent *a, ...); /* It computes the Dolan's function */
+double Easom(Agent *a, ...); /* It computes the Easom's function */
+double ElAttar_VidyasagarDutta(Agent *a, ...); /* It computes the El-Attar-Vidyasagar-Dutta function */
+double Eggcrate(Agent *a, ...); /* It computes the Egg Crate function */
+double Eggholder(Agent *a, ...); /* It computes the Egg Holder function */
+double Exponential(Agent *a, ...); /* It computes the Exponential function */
+double EXP_2(Agent *a, ...); /* It computes the 2-D Exponential function */
+double Forrester(Agent *a, ...); /* It computes the Forrester et al. (2008) function */
+double Freudenstein_Roth(Agent *a, ...); /* It computes the Freudenstein's Roth function */
+double Giunta(Agent *a, ...); /* It computes the Giunta's function */
+double Goldstein_Price(Agent *a, ...); /* It computes the Goldstein's-Price function */
+double Gramacy_Lee(Agent *a, ...); /* It computes the Gramacy & Lee's (2012) function */
+double Griewank(Agent *a, ...); /* It computes the Griewank's function */
+double Gulf_Research(Agent *a, ...); /* It computes the Gulf's Research problem */
+double Hansen(Agent *a, ...); /* It computes the Hansen's function */
+double Hartmann_3(Agent *a, ...); /* It computes the Hartmann's 3-D function */
+double Hartmann_6(Agent *a, ...); /* It computes the Hartmann's 6-D function */
+double Helical_Valley(Agent *a, ...); /* It computes the Helical Valley function */
+double Himmelblau(Agent *a, ...); /* It computes the Himmelblau's function */
+double Hosaki(Agent *a, ...); /* It computes the Hosaki's function */
+double Jennrick_Sampson(Agent *a, ...); /* It computes the Jennrick's-Sampson function */
+double Keane(Agent *a, ...); /* It computes the Keane's function */
+double Langermann_5(Agent *a, ...); /* It computes the Langermann's function */
+double Leon(Agent *a, ...); /* It computes the Leon's function */
+double Levy(Agent *a, ...); /* It computes the Levy's function */
+double Levy_Thirteenth(Agent *a, ...); /* It computes the 13th Levy's function */
+double Matyas(Agent *a, ...); /* It computes the Matyas' function */
+double McCormick(Agent *a, ...); /* It computes the McCormick's function */
+double Miele_Cantrell(Agent *a, ...); /* It computes the Miele's Cantrell function */
+double Mishra_1(Agent *a, ...); /* It computes the 1st Mishra function */
+double Mishra_2(Agent *a, ...); /* It computes the 2nd Mishra function */
+double Mishra_3(Agent *a, ...); /* It computes the 3rd Mishra function */
+double Mishra_4(Agent *a, ...); /* It computes the 4th Mishra function */
+double Mishra_5(Agent *a, ...); /* It computes the 5th Mishra function */
+double Mishra_6(Agent *a, ...); /* It computes the 6th Mishra function */
+double Mishra_7(Agent *a, ...); /* It computes the 7th Mishra function */
+double Mishra_8(Agent *a, ...); /* It computes the 8th Mishra function */
+double Mishra_9(Agent *a, ...); /* It computes the 9th Mishra function */
+double Mishra_10(Agent *a, ...); /* It computes the 10th Mishra function */
+double Mishra_11(Agent *a, ...); /* It computes the 11st Mishra function */
+double Parsopoulos(Agent *a, ...); /* It computes the Parsopoulos' function */
+double Pen_Holder(Agent *a, ...); /* It computes the Pen Holder function */
+double Pathological(Agent *a, ...); /* It computes the Pathological function */
+double Paviani(Agent *a, ...); /* It computes the Paviani's function */
+double Pinter(Agent *a, ...); /* It computes the Pintér's function */
+double Periodic(Agent *a, ...); /* It computes the Periodic function */
+double Powell_Singular_First(Agent *a, ...); /* It computes the 1st Powell's Singular function */
+double Powell_Singular_Second(Agent *a, ...); /* It computes the 2nd Powell's Singular function */
+double Powell_Sum(Agent *a, ...); /* It computes the Powell's Sum function */
+double Price_First(Agent *a, ...); /* It computes the 1st Price's function */
+double Price_Second(Agent *a, ...); /* It computes the 2nd Price's function */
+double Price_Third(Agent *a, ...); /* It computes the 3rd Price's function */
+double Price_Forth(Agent *a, ...); /* It computes the 4th Price's function */
+double Qing(Agent *a, ...); /* It computes the Qing's function */
+double Quadratic(Agent *a, ...); /* It computes the Quadratic function */
+double Quartic(Agent *a, ...); /* It computes the Quartic function */
+double Quintic(Agent *a, ...); /* It computes the Quintic function */
+double Rana(Agent *a, ...); /* It computes the Rana's function */
+double Rastrigin(Agent *a, ...); /* It computes the Rastrigin's function */
+double Ripple_1(Agent *a, ...); /* It computes the Ripple 1 function */
+double Ripple_25(Agent *a, ...); /* It computes the Ripple 25 function */
+double Rosenbrock(Agent *a, ...); /* It computes the Rosenbrock's function */
+double Rosenbrock_Modified(Agent *a, ...); /* It computes the Rosenbrock's Modified function */
+double Rotated_Ellipsoid_1(Agent *a, ...); /* It computes the Rotated Ellipsoid 1 function */
+double Rotated_Ellipsoid_2(Agent *a, ...); /* It computes the Rotated Ellipsoid 2 function */
+double Rump(Agent *a, ...); /* It computes the Rump's function */
+double Salomon(Agent *a, ...); /* It computes the Salomon's function */
+double Sargan(Agent *a, ...); /* It computes the Sargan's function */
+double Schaffer_First(Agent *a, ...); /* It computes the 1st Schaffer's function */
+double Schaffer_Second(Agent *a, ...); /* It computes the 2nd Schaffer's function */
+double Schaffer_Third(Agent *a, ...); /* It computes the 3rd Schaffer's function */
+double Schaffer_Forth(Agent *a, ...); /* It computes the 4th Schaffer's function */
+double Schmidt_Vetters(Agent *a, ...); /* It computes the Schmidt's Vetters function */
+double Schumer_Steiglitz(Agent *a, ...); /* It computes the Schumer's Steiglitz function */
+double Schwefel(Agent *a, ...); /* It computes the Schewefel's function */
+double Shubert_First(Agent *a, ...); /* It computes the 1st Shubert's function */
+double Shubert_Third(Agent *a, ...); /* It computes the 3rd Shubert's function */
+double Shubert_Forth(Agent *a, ...); /* It computes the 4th Shubert's function */
+double Sphere(Agent *a, ...); /* It computes the Sphere's function */
+double Step_1(Agent *a, ...); /* It computes the Step 1 function */
+double Step_2(Agent *a, ...); /* It computes the Step 2 function */
+double Step_3(Agent *a, ...); /* It computes the Step 3function */
+double Stepint(Agent *a, ...); /* It computes the Stepint's function */
+double Streched_V_SineWave(Agent *a, ...); /* It computes the Streched V Sine Wave function */
+double Sum_DifferentPowers(Agent *a, ...); /* It computes the Sum of Different Powers function */
+double Sum_Squares(Agent *a, ...); /* It computes the Sum Squares function */
+double Styblinski_Tang(Agent *a, ...); /* It computes the Styblinski's-Tang function */
+double Holder_Table_First(Agent *a, ...); /* It computes the 1st Holder's Table function */
+double Holder_Table_Second(Agent *a, ...); /* It computes the 2nd Holder's Table function */
+double Carrom_Table(Agent *a, ...); /* It computes the Carrom's Table function */
+double Testtube_Holder(Agent *a, ...); /* It computes the Testtube's Holder function */
+double Trecanni(Agent *a, ...); /* It computes the Trecanni's function */
+double Trid_6(Agent *a, ...); /* It computes the Trid 6 function */
+double Trid_10(Agent *a, ...); /* It computes the Trid 10 function */
+double Trefethen(Agent *a, ...); /* It computes the Trefethen's function */
+double Trigonometric_1(Agent *a, ...); /* It computes the Trigonometric 1 function */
+double Trigonometric_2(Agent *a, ...); /* It computes the Trigonometric 2 function */
+double Tripod(Agent *a, ...); /* It computes the Tripod's function */
+double Ursem_1(Agent *a, ...); /* It computes the Ursem 1 function */
+double Ursem_3(Agent *a, ...); /* It computes the Ursem 3 function */
+double Ursem_4(Agent *a, ...); /* It computes the Ursem 4 function */
+double Ursem_Waves(Agent *a, ...); /* It computes the Ursem Waves function */
+double Venter_Sobiezcczanski(Agent *a, ...); /* It computes the Venter's Sobiezcczanski-Sobieski function */
+double Watson(Agent *a, ...); /* It computes the Watson's function */
+double Wayburn_Seader_1(Agent *a, ...); /* It computes the Wayburn Seader 1 function */
+double Wayburn_Seader_2(Agent *a, ...); /* It computes the Wayburn Seader 2 function */
+double Wayburn_Seader_3(Agent *a, ...); /* It computes the Wayburn Seader 3 function */
+double Wavy(Agent *a, ...); /* It computes the Wavy's function */
+double Weierstrass(Agent *a, ...); /* It computes the Weierstrass' function */
+double Whitley(Agent *a, ...); /* It computes the Whitley's function */
+double Wolfe(Agent *a, ...); /* It computes the Wolfe's function */
+double XinShe_Yang_1(Agent *a, ...); /* It computes the Xin-She Yang's 1 function */
+double XinShe_Yang_2(Agent *a, ...); /* It computes the Xin-She Yang's 2 function */
+double XinShe_Yang_3(Agent *a, ...); /* It computes the Xin-She Yang's 3 function */
+double XinShe_Yang_4(Agent *a, ...); /* It computes the Xin-She Yang's 4 function */
+double Zakharov(Agent *a, ...); /* It computes the Zakharov's function */
+double Zettl(Agent *a, ...); /* It computes the Zettl's function */
+double Zirilli(Agent *a, ...); /* It computes the Zirilli's function */
 
 #endif
--- src/function.c
+++ src/function.c
@@ -1,28 +1,111 @@
 #include "function.h"
 
-/* It computes the equation of a 2-D sphere
+/* It computes the Ackley's function
 Parameters:
 a: agent
 ---
-Minimum at f(0,0) = 0
+Minimum at f(x*) = 0 at x_i = 0
 Domain: -infty < x,y < +infty */
-double Sphere(Agent *a, ...){
+double Ackley(Agent *a, ...){
     int i;
-    double sum = 0;
+    double alpha = 20, beta = 0.2, gamma = 2*M_PI;
+    double sum = 0, sum_1 = 0, sum_2 = 0;
     
     if(!a){
-        fprintf(stderr,"\nAgent not allocated @Sphere.\n");
+        fprintf(stderr,"\nAgent not allocated @Ackley.\n");
         return DBL_MAX;
     }
     
     if(a->n < 1){
-        fprintf(stderr,"\nInvalid number of decision variables @Sphere. It must be greater than one.\n");
+        fprintf(stderr,"\nInvalid number of decision variables @Ackley. It must be greater than one.\n");
         return DBL_MAX;
     }
     
-    for (i = 0; i < a->n; i++)
-	sum += pow(a->x[i], 2); 
-    	
+    for (i = 0; i < a->n; i++){
+	sum_1 += pow(a->x[i], 2);
+	sum_2 += cos(gamma*a->x[i]);
+    }
+    
+    sum = -alpha*exp(-beta*sqrt((1/a->n)*sum_1)) - exp((1/a->n)*sum_2) + alpha + exp(1);
+	
+    return sum;
+}
+
+/* It computes the sixth Bukin's function
+Parameters:
+a: agent
+---
+Minimum at f(x*) = 0 at x* = (-10, 1)
+Domain: -infty < x,y < +infty */
+double Bukin_Sixth(Agent *a, ...){
+    int i;
+    double sum = 0;
+    
+    if(!a){
+        fprintf(stderr,"\nAgent not allocated @Bukin_Sixth.\n");
+        return DBL_MAX;
+    }
+    
+    if(a->n != 2){
+        fprintf(stderr,"\nInvalid number of decision variables @Bukin_Sixth. It must be equal to two.\n");
+        return DBL_MAX;
+    }
+    
+    sum = 100 * sqrt(fabs(a->x[1] - 0.01*pow(a->x[0], 2))) + 0.01 * fabs(a->x[0] + 10);
+    
+    return sum;
+}
+
+/* It computes the Cross-in-Tray function
+Parameters:
+a: agent
+---
+Minimum at f(x*) = -2.06261 at x = (1.3491, -1.3491), (1.3491, 1.3491), (-1.3491, 1.3491) and (-1.3491, -1.3491)
+Domain: -infty < x,y < +infty */
+double Cross_Tray(Agent *a, ...){
+    int i;
+    double sum = 0;
+    
+    if(!a){
+        fprintf(stderr,"\nAgent not allocated @Cross_Tray.\n");
+        return DBL_MAX;
+    }
+    
+    if(a->n != 2){
+        fprintf(stderr,"\nInvalid number of decision variables @Cross_Tray. It must be equal to two.\n");
+        return DBL_MAX;
+    }
+    
+    sum = -0.0001 * pow((fabs(sin(a->x[0])*sin(a->x[1])*exp(fabs(100 - (sqrt(pow(a->x[0], 2)+pow(a->x[1], 2))/M_PI)))) + 1), 0.1);
+    
+    return sum;
+}
+
+/* It computes the Dixon-Price's function
+Parameters:
+a: agent
+---
+Minimum at f(x*) = 0 at x_i = 2^{-/frac{2^i-2}{2^i}}
+Domain: -infty < x,y < +infty */
+double Dixon_Price(Agent *a, ...){
+    int i;
+    double sum = 0;
+    
+    if(!a){
+        fprintf(stderr,"\nAgent not allocated @Dixon_Price.\n");
+        return DBL_MAX;
+    }
+    
+    if(a->n < 1){
+        fprintf(stderr,"\nInvalid number of decision variables @Dixon_Price. It must be greater than one.\n");
+        return DBL_MAX;
+    }
+    
+    sum = pow(a->x[0] - 1, 2);
+    
+    for (i = 1; i < a->n; i++)
+	sum += (i+1) * pow((2 * pow(a->x[i], 2) - a->x[i-1]), 2);
+	
     return sum;
 }
 
@@ -52,30 +135,28 @@ double Rosenbrock(Agent *a, ...){
     return sum;
 }
 
-/* It computes the Dixon-Price's function
+/* It computes the Sphere's function
 Parameters:
 a: agent
 ---
-Minimum at f(x*) = 0 at x_i = 2^{-/frac{2^i-2}{2^i}}
+Minimum at f(0,0) = 0
 Domain: -infty < x,y < +infty */
-double Dixon_Price(Agent *a, ...){
+double Sphere(Agent *a, ...){
     int i;
     double sum = 0;
     
     if(!a){
-        fprintf(stderr,"\nAgent not allocated @Dixon_Price.\n");
+        fprintf(stderr,"\nAgent not allocated @Sphere.\n");
         return DBL_MAX;
     }
     
     if(a->n < 1){
-        fprintf(stderr,"\nInvalid number of decision variables @Dixon_Price. It must be greater than one.\n");
+        fprintf(stderr,"\nInvalid number of decision variables @Sphere. It must be greater than one.\n");
         return DBL_MAX;
     }
     
-    sum = pow(a->x[0] - 1, 2);
-    
-    for (i = 1; i < a->n; i++)
-	sum += (i+1) * pow((2 * pow(a->x[i], 2) - a->x[i-1]), 2);
-	
+    for (i = 0; i < a->n; i++)
+	sum += pow(a->x[i], 2); 
+    	
     return sum;
 }
