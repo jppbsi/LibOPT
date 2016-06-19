fc54e80c385b57ea4db13dc1f5914163f58c9f3b
gugarosa <gth.rosa@uol.com.br>
2016-06-19 12:59:24 -0300

    Added missing function on CS header.

--- include/cs.h
+++ include/cs.h
@@ -1,4 +1,4 @@
-/* CS implementation is based on the paper available at  */
+/* CS implementation is based on the paper available at https://arxiv.org/pdf/1003.1594.pdf */
 
 #ifndef CS_H
 #define CS_H
@@ -6,6 +6,7 @@
 #include "opt.h"
 #include "common.h"
 
+int NestLossParameter(int size, float probability); /* It computes the number of nests that will be replaced, taking into account a probability [0,1] */
 void runCS(SearchSpace *s, prtFun Evaluate, ...); /* It executes the Cuckoo Search for function minimization */
 
 #endif
--- src/cs.c
+++ src/cs.c
@@ -1,6 +1,6 @@
 #include "cs.h"
 
-/*It computes the number of nests that will be replaced, taking into account a probability[0,1]*/
+/* It computes the number of nests that will be replaced, taking into account a probability [0,1] */
 int NestLossParameter(int size, float probability){
     int value, loss;
     value = size*probability;
@@ -33,39 +33,40 @@ void runCS(SearchSpace *s, prtFun Evaluate, ...){
     }
         
     EvaluateSearchSpace(s, _CS_, Evaluate, arg); /* Initial evaluation of the search space */
-    
-	qsort(s->a, s->m, sizeof(Agent**), SortAgent); /* Sorts all nests according to their fitness. First position gets the best nest. */
+    qsort(s->a, s->m, sizeof(Agent**), SortAgent); /* Sorts all nests according to their fitness. First position gets the best nest. */
     
     for(t = 1; t <= s->iterations; t++){
     	fprintf(stderr,"\nRunning iteration %d/%d ... ", t, s->iterations);
             
     	va_copy(arg, argtmp);
-		tmp = GenerateNewAgent(s, _CS_);
-		
-		/* Equation 1 */
-		L = GenerateLevyDistribution(s->n, s->beta);
-		for(j = 0; j < s->n; j++)
-			tmp->x[j] = tmp->x[j]+L[j]*(tmp->x[j]-s->g[j]);
-		free(L);
-		/**************/
-		CheckAgentLimits(s, tmp);
-		
-		nest_i = GenerateUniformRandomNumber(0, s->m-1);
-		
-		fitValue = Evaluate(tmp, arg); /* It executes the fitness function for agent i */
-		if(fitValue < s->a[nest_i]->fit){ /* We accept the new solution */
-			DestroyAgent(&(s->a[nest_i]), _CS_);
-			s->a[nest_i] = CopyAgent(tmp, _CS_);
-			s->a[nest_i]->fit = fitValue;
-		}
-		
-		DestroyAgent(&tmp, _CS_);
-		
-		
-		qsort(s->a, s->m, sizeof(Agent**), SortAgent); /* Sorts all nests according to their fitness. First position gets the best nest. */
-		
-		va_copy(arg, argtmp);
-		fprintf(stderr, "OK (minimum fitness value %lf)", s->a[0]->fit);
+	tmp = GenerateNewAgent(s, _CS_);
+	
+	/* Equation 1 */
+	L = GenerateLevyDistribution(s->n, s->beta);
+	for(j = 0; j < s->n; j++)
+		tmp->x[j] = tmp->x[j]+L[j]*(tmp->x[j]-s->g[j]);
+	free(L);
+	
+	/**************/
+	
+	CheckAgentLimits(s, tmp);
+	
+	nest_i = GenerateUniformRandomNumber(0, s->m-1);
+	
+	fitValue = Evaluate(tmp, arg); /* It executes the fitness function for agent i */
+	if(fitValue < s->a[nest_i]->fit){ /* We accept the new solution */
+	    DestroyAgent(&(s->a[nest_i]), _CS_);
+	    s->a[nest_i] = CopyAgent(tmp, _CS_);
+	    s->a[nest_i]->fit = fitValue;
 	}
-	va_end(arg);
+	
+	DestroyAgent(&tmp, _CS_);
+	
+	qsort(s->a, s->m, sizeof(Agent**), SortAgent); /* Sorts all nests according to their fitness. First position gets the best nest. */
+	
+	va_copy(arg, argtmp);
+	fprintf(stderr, "OK (minimum fitness value %lf)", s->a[0]->fit);
+    }
+    
+    va_end(arg);
 }
