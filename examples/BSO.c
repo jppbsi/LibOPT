4d9c8a98c4851213a6d6b5569bc17c8196f6ccc1
Joao Papa <papa@Joaos-MacBook-Air-2.local>
2017-07-27 09:25:09 -0300

    Working on BSO.

--- examples/BSO.c
+++ examples/BSO.c
@@ -9,12 +9,12 @@ int main(){
     
     s = ReadSearchSpaceFromFile("examples/model_files/bso_model.txt", _BSO_); /* It reads the model file and creates a search space. We are going to use PSO to solve our problem. */
     
-    //InitializeSearchSpace(s, _PSO_); /* It initalizes the search space */
+    InitializeSearchSpace(s, _BSO_); /* It initalizes the search space */
     
     //if (CheckSearchSpace(s, _PSO_)) /* It checks wether the search space is valid or not */
       //  runPSO(s, Sphere); /* It minimizes function Sphere */
     
-    DestroySearchSpace(&s, _PSO_); /* It deallocates the search space */
+    DestroySearchSpace(&s, _BSO_); /* It deallocates the search space */
         
     return 0;
 }
