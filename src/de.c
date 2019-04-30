21c05866ec430582276ae72a6d17e71de0e0268b
Gustavo Rosa <gth.rosa@uol.com.br>
2019-04-30 13:34:21 -0300

    Fixing up DE function arguments.

--- src/de.c
+++ src/de.c
@@ -1,29 +1,27 @@
 #include "de.h"
 #include "function.h"
 
-void MutationAndRecombination(SearchSpace *searchSpace, int target, prtFun function, ...) {
-    va_list args;
+void MutationAndRecombination(SearchSpace *searchSpace, int target, prtFun function, va_list arg) {
     Agent *mutant;
     int a, b, c, k;
 
-    va_start(args, function);
-
     mutant = GenerateNewAgent(searchSpace, _DE_);
     a = b = c = target;
-
     while (a == target) a = GenerateUniformRandomNumber(0, 1) * searchSpace->m;
     while (b == target || b == a) b = GenerateUniformRandomNumber(0, 1) * searchSpace->m;
     while (c == target || c == b || c == a) c = GenerateUniformRandomNumber(0, 1) * searchSpace->m;
-
     for (k = 0; k < searchSpace->n; k++) {
+
         mutant->x[k] = GenerateUniformRandomNumber(0, 1) < searchSpace->cross_probability
                        ? searchSpace->a[target]->x[k]
                        : searchSpace->a[a]->x[k]
                          + searchSpace->mutation_factor
                            * (searchSpace->a[b]->x[k] - searchSpace->a[c]->x[k]);
-    }
 
-    mutant->fit = function(mutant, args);
+    }
+    CheckAgentLimits(searchSpace, mutant);
+
+    mutant->fit = function(mutant, arg);
 
     if(mutant->fit < searchSpace->a[target]->fit) {
         DestroyAgent(&searchSpace->a[target], _DE_);
@@ -35,38 +33,44 @@ void MutationAndRecombination(SearchSpace *searchSpace, int target, prtFun funct
 
 /**
  * It executes the Differential Evolution for function minimization
- * @param searchSpace Search space
+ * @param s Search space
  * @param prtFunction Pointer to the function used to evaluate particles
  * @param ... List of additional arguments
  */
-void runDE(SearchSpace *searchSpace, prtFun function, ...) {
+void runDE(SearchSpace *s, prtFun Evaluate, ...) {
     int t, k;
-    va_list args, initialArgs;
+    va_list arg, argtmp;
 
-    va_start(args, function);
-    va_copy(initialArgs, args);
+    va_start(arg, Evaluate);
+    va_copy(argtmp, arg);
 
-    if (!searchSpace) {
+    if (!s) {
         fprintf(stderr, "\nSearch space not allocated @runDE.\n");
         exit(-1);
     }
 
-    EvaluateSearchSpace(searchSpace, _DE_, function, args); /* Initial evaluation */
-
-    for (t = 1; t <= searchSpace->iterations; t++) {
-        fprintf(stderr, "\nRunning iteration %d/%d ... ", t, searchSpace->iterations);
-        va_copy(initialArgs, args);
-
-        /* For each particle */
-        for (k = 0; k < searchSpace->m; k++) {
-            MutationAndRecombination(searchSpace, k, function, args);
-
-            EvaluateSearchSpace(searchSpace, _DE_, function, args);
-        }
-
-        fprintf(stderr, "OK (minimum fitness value %lf)", searchSpace->gfit);
+    if (s->m<=3) {
+        fprintf(stderr, "\nNumber of agents should be greater than 3 @runDE.\n");
+        exit(-1);
     }
 
-    va_end(args);
-}
+    EvaluateSearchSpace(s, _DE_, Evaluate, arg); /* Initial evaluation */
 
+    for (t = 1; t <= s->iterations; t++) {
+        fprintf(stderr, "\nRunning iteration %d/%d ... ", t, s->iterations);
+
+
+        /* For each particle */
+        for (k = 0; k < s->m; k++) {
+			va_copy(arg, argtmp);
+           	MutationAndRecombination(s, k, Evaluate, arg);
+        }
+		va_copy(arg, argtmp);
+        EvaluateSearchSpace(s, _DE_, Evaluate, arg);
+
+        fprintf(stderr, "OK (minimum fitness value %lf)", s->gfit);
+		fprintf(stdout, "\n%d %lf",t, s->gfit);
+    }
+
+    va_end(arg);
+}
