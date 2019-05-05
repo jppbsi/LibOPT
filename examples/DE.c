dc306498d80b48c4487ba5b32386562caee17700
maykon <maykonmichelpalma@gmail.com>
2019-05-05 19:35:01 -0300

    Updated variables to lib pattern

--- examples/DE.c
+++ examples/DE.c
@@ -3,17 +3,17 @@
 #include "de.h"
 
 int main() {
-    SearchSpace *searchSpace = NULL;
+    SearchSpace *s = NULL;
 
     /* It reads the model file and creates a search space. We are going to use LOA to solve our problem. */
-    searchSpace = ReadSearchSpaceFromFile("examples/model_files/de_model.txt", _DE_);
+    s = ReadSearchSpaceFromFile("examples/model_files/de_model.txt", _DE_);
 
-    InitializeSearchSpace(searchSpace, _DE_);   /* It initializes the search space */
+    InitializeSearchSpace(s, _DE_);   /* It initializes the search space */
 
-    if (CheckSearchSpace(searchSpace, _DE_))    /* It checks whether the search space is valid or not */
-        runDE(searchSpace, Sphere);             /* It minimizes function Sphere */
+    if (CheckSearchSpace(s, _DE_))    /* It checks whether the search space is valid or not */
+        runDE(s, Sphere);             /* It minimizes function Sphere */
 
-    DestroySearchSpace(&searchSpace, _DE_);     /* It de-allocates the search space */
+    DestroySearchSpace(&s, _DE_);     /* It de-allocates the search space */
 
     return 0;
 }
